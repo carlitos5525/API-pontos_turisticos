451894ec8799c3b43c64875d661dbc96e4902a9f
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-05-20 21:16:27 -0300

    Teste com action personalizadas

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,8 +1,30 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from core.api.serializers import PontoTuristicoSerializer
 
 
 class PontoTuristicoViewSet(ModelViewSet):
-    queryset = PontoTuristico.objects.all()
     serializer_class = PontoTuristicoSerializer
+
+    def get_queryset(self):
+        return PontoTuristico.objects.filter(is_approved=True)
+
+    # def list(self, request, *args, **kwargs):
+        # return Response({'teste': 123})
+
+    # def create(self, request):
+        # return Response({'Hello': request.data['nome']})
+
+    #def destroy(self, request, *args, **kwargs):
+        #pass
+
+    # criando uma action personalizada
+    # detail=True serve para que o framework envie a pk para action
+    # endpoint para acessar /pontoturistico/1/denunciar/
+    @action(methods=['get'], detail=True)
+    def denunciar(self, request, pk):
+        return Response({'teste': 'deu boa'})
+
+
