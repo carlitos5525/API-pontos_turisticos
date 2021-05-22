6a1385a59a53ff0492536e929728e8e394ec924f
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-05-22 19:46:12 -0300

    Add djangofilterbackend to Atracoes ViewSet

--- api_pontos_turisticos/settings.py
+++ api_pontos_turisticos/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'atracoes',
     'enderecos',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
--- atracoes/api/viewsets.py
+++ atracoes/api/viewsets.py
@@ -1,8 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 from atracoes.models import Atracao
 from .serializers import AtracoesSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 
 class AtracaoViewSet(ModelViewSet):
     queryset = Atracao.objects.all()
     serializer_class = AtracoesSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'description']
+
