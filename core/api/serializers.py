0d1838b2949e8f90f9e47230e1d1bae39386cf16
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-05-23 20:18:10 -0300

    Add complete_description to PontoTuristico

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import SerializerMethodField
 from core.models import PontoTuristico
 from atracoes.api.serializers import AtracoesSerializer
 from comentarios.api.serializers import ComentarioSerializer
@@ -11,10 +12,13 @@ class PontoTuristicoSerializer(ModelSerializer):
     comentarios = ComentarioSerializer(many=True)
     avaliacoes = AvaliacaoSerializer(many=True)
     enderecos = EnderecoSerializer()
-
+    complete_description = SerializerMethodField()
 
     class Meta:
         model = PontoTuristico
         fields = ('id', 'name', 'description', 'is_approved', 'photo',
-                  'atracoes', 'comentarios', 'avaliacoes', 'enderecos')
+                  'atracoes', 'comentarios', 'avaliacoes', 'enderecos',
+                  'complete_description')
 
+    def get_complete_description(self, obj):
+        return f'{obj.name} - {obj.description}'
