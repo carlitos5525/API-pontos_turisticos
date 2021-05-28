f76990135048f6ee3525edf68d888716f83c37a9
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-05-27 21:06:42 -0300

    Add pagination

--- api_pontos_turisticos/settings.py
+++ api_pontos_turisticos/settings.py
@@ -138,5 +138,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5
 }
